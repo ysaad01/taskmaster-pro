df31f8624a64164474885b147e2281076e79d123
ysaad01 <ysaad2325@gmail.com>
2022-03-02 22:03:27 -0500

    drag and drop feature added

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,6 +42,78 @@ var loadTasks = function() {
   });
 };
 
+/* Enable Draggable/Sortable feature on list-group elements */
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event) {
+    // console.log("activate", this);
+  },
+
+  deactivate: function (event) {
+    // console.log("deactivate", this);
+  },
+
+  over: function (event) {
+    // console.log("over", event.target);
+  },
+
+  out: function (event) {
+    // console.log("out", event.target);
+  },
+
+  update: function (event) {
+    /* array to store the task data */
+    var tempArr = [];
+
+    /* loop over current set of children in sortable list */
+    $(this)
+      .children()
+      .each(function () {
+        /* save values in temp array */
+        tempArr.push({
+          text: $(this)
+            .find("p")
+            .text()
+            .trim(),
+      
+          date: $(this)
+            .find("span")
+            .text()
+            .trim()
+        });
+      });
+    
+    /* trim down list's ID to match object property */
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+    
+    /* update array on tasks object and save */
+    tasks[arrName] = tempArr;
+    saveTasks();
+  },
+  stop: function (event) {
+    $(this).removeClass("dropover");
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function (event, ui) {
+    ui.draggable.remove();
+  },
+  over: function (event, ui) {
+    console.log(ui);
+  },
+  out: function (event, ui) {
+    console.log(ui);
+  }
+});
+
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
